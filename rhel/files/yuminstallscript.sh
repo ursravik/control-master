bef852bd4417211b105ab318710cebd7fdf267d6
ursravik <ursravik@gmail.com>
2019-06-08 13:36:30 -0400

    rhel patching playbook

--- rhel/files/yuminstallscript.sh
+++ rhel/files/yuminstallscript.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 [[ -f /tmp/yumsuccess ]] && rm /tmp/yumsuccess
-yum update libblkid  -y > /tmp/yumresult.txt
+[[ -f /tmp/yumresult.txt ]] && rm /tmp/yumresult.txt
+yum update -y > /tmp/yumresult.txt
 grep "Complete!" /tmp/yumresult.txt
 if [ $? == 0 ]
 then
